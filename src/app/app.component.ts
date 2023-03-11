40b1ae69bebd06220ab815fbec0cc2fbefa3cdb0
Michael Seemann <michael.seemann@synaos.com>
2023-03-11 11:37:06 +0100

    playwright added without browsers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'apw';
+  showText = false;
+
+  userDidClickButton() {
+    this.showText = !this.showText
+  }
 }
